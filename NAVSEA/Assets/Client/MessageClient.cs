e1e49f9dc5d0949ffb06769a45eff2c95bc093a5
thaophan1 <thao_phan@ucsb.edu>
2021-11-02 21:31:39 -0700

    Add new client module

---
+++ NAVSEA/Assets/Client/MessageClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace Assets.Client
+{
+    class MessageClient : IDisposable
+    {
+        private UdpClient client;
+
+        public MessageClient()
+        {
+            client = new UdpClient();
+            Debug.Log("Initialized Unity UDP Client");
+            client.Connect("127.0.0.1", 10000);
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+
+        public void SendMessage(string message)
+        {
+            Byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+            client.Send(messageBytes, messageBytes.Length);
+        }
+
+        
+    }
+}
