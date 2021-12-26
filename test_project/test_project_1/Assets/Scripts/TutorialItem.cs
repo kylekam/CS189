c658c52ef548ba656f036c21f1e02755610bc0f0
Vivian Ross <vivianross@ucsb.edu>
2021-12-25 21:22:03 -0800

    added prefabs and some code for instructions

---
+++ test_project/test_project_1/Assets/Scripts/TutorialItem.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+using TMPro;
+
+public class TutorialItem : ScaleTween
+{
+    public enum ContinueType { OkButton, Hand, None }
+
+    [Header("Visual Components")]
+    [SerializeField] private TextMeshPro bodyTextMesh;
+    [SerializeField] private TextMeshPro stepTextMesh;
+    [SerializeField] private GameObject okButton;
+    [SerializeField] private GameObject handSymbol;
+    public ContinueType continueType;
+
+    [Header("Display Location")]
+    [SerializeField] Transform displayParent;
+
+    // *** ITutorialItem Implementation ***
+    [SerializeField] private int m_itemOrder;
+    public int ItemOrder
+    {
+        get => m_itemOrder;
+    }
+    // *** End ITutorialItem ***
+    public BoolVariable CloseCondition;
+
+    public static List<TutorialItem> GetTutorialItems()
+    {
+        return Resources.FindObjectsOfTypeAll<TutorialItem>()
+            .Where(item => item.gameObject.scene.IsValid())
+            .OrderBy(item => item.ItemOrder)
+            .ToList();
+    }
+
+    public void OnValidate()
+    {
+        if (stepTextMesh != null)
+        {
+            int totalItems = GetTutorialItems().Count;
+            string stepStr = $"{m_itemOrder.ToString()} / {totalItems.ToString()}";
+            stepTextMesh.text = stepStr;
+        }
+
+        okButton?.SetActive(false);
+        handSymbol?.SetActive(false);
+        if (continueType == ContinueType.OkButton) { okButton?.SetActive(true); }
+        else if (continueType == ContinueType.Hand) { handSymbol?.SetActive(true); }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Open()
+    {
+        if (displayParent == null)
+        { 
+            Debug.Log($"{gameObject.name} has no displayParent");    
+        }
+        else
+        { 
+            this.transform.SetPositionAndRotation(displayParent.transform.position, displayParent.parent.rotation);
+            // this.transform.parent = displayParent;
+            this.transform.parent = displayParent;
+            this.transform.localPosition = Vector3.zero;
+        }
+        gameObject.SetActive(true);
+        base.TweenIn();
+    }
+
+    public virtual void Close()
+    {
+        base.TweenOut(delegate ()
+        {
+            gameObject.SetActive(false);
+        });
+    }
+
+    public void OnTutorialEnter()
+    {
+        Debug.Log($"[Tutorial Item {ItemOrder}]");
+
+    }
+
+    public void OnTutorialExit()
+    {
+
+    }
+}
---
+++ test_project/test_project_1/Assets/Scripts/TutorialMainActivity.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialMainActivity : MonoBehaviour
+{
+    private List<TutorialItem> tutorialItems;
+    private TutorialItem currentItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"TutorialMainActivity Start");
+        StartActivity();
+        // tutorialItems = TutorialItem.GetTutorialItems();
+        // foreach(TutorialItem item in tutorialItems)
+        // {
+        //     item.OnTweenInComplete.AddListener(
+        //         delegate
+        //         {
+
+        //         }
+        //     );
+
+        //     ARDebug.logToUnityConsole = true;
+        //     ARDebug.Log($"[Tutorial Item {item.ItemOrder}] TweenIn Callbacks: {item.OnTweenInComplete.GetPersistentEventCount()}");
+        //     ARDebug.logToUnityConsole = false;
+
+        //     item.OnTweenOutComplete.AddListener(DoTutorialItemExit);
+        // }
+    }
+
+    // Update is called once per frame
+    void Update() {}
+
+    public void StartActivity()
+    {
+        tutorialItems = TutorialItem.GetTutorialItems();
+        currentItem = tutorialItems[0];
+        currentItem.Open();
+        currentItem.OnTutorialEnter();
+    }
+
+    public void StopActivity()
+    {
+        tutorialItems = TutorialItem.GetTutorialItems();
+        foreach(var tutorialItem in tutorialItems)
+        {
+            tutorialItem.Close();
+            tutorialItem.OnTutorialExit();
+            currentItem = null;
+        }
+    }
+
+    public void NextItem()
+    {
+        // Close current step
+        currentItem.Close();
+        currentItem.OnTutorialExit();
+        tutorialItems.Remove(currentItem);
+
+        // Start next step
+        if (tutorialItems.Count > 0)
+        {
+            currentItem = tutorialItems[0];
+            if (currentItem.CloseCondition != null)
+            {
+                currentItem.CloseCondition.OnValueChanged += CloseConditionHandler;
+            }
+            currentItem.Open();
+            currentItem.OnTutorialEnter();
+        }
+    }
+
+    public void CloseConditionHandler(bool newValue)
+    {
+        if (newValue == true)
+        { 
+            currentItem.CloseCondition.OnValueChanged -= CloseConditionHandler;
+            NextItem();
+        }
+    }
+
+    // public void OnItemOpen()
+    // {
+    //     AudioSource audio = currentItem.gameObject.GetComponent<AudioSource>();
+    //     audio.clip = currentItem.dialogue.audioClip;
+    //     audio.Play();
+    // }
+
+    // public void OnItemClose()
+    // {
+    //     AudioSource audio = currentItem.gameObject.GetComponent<AudioSource>();
+    //     audio.Stop();
+    // }
+}
