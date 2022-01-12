d593b4cde53f17e478c12884cd9d278afddabdc3
thaophan1 <thao_phan@ucsb.edu>
2022-01-11 16:18:19 -0800

    GameObjects change color when being HeadGazed at

--- NAVSEA/test_project_1/Assets/Scripts/EyeTracking.cs
+++ NAVSEA/test_project_1/Assets/Scripts/EyeTracking.cs
@@ -6,6 +6,8 @@ using Microsoft.MixedReality.Toolkit.Input;
 public class EyeTracking : MonoBehaviour
 {
     private GazeProvider gazeProvider;
+    private GameObject lastGameObject;
+    private Color lastColor;
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +31,37 @@ public class EyeTracking : MonoBehaviour
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo))
         {
             GameObject go = hitInfo.collider.gameObject;
+            if (lastGameObject == null)
+            {
+                var renderer = go.GetComponent<Renderer>();
+
+                lastGameObject = go;
+                lastColor = renderer.material.color;
+                renderer.material.SetColor("_Color", Color.green);
+            } else if (go != lastGameObject)
+            {
+                var renderer = lastGameObject.GetComponent<Renderer>();
+                renderer.material.SetColor("_Color", lastColor);
+
+                renderer = go.GetComponent<Renderer>();
+                lastGameObject = go;
+                lastColor = renderer.material.color;
+                renderer.material.SetColor("_Color", Color.green);
+            }
+
             print(hitInfo.point);
             if (go.CompareTag("Board"))
             {
                 print("HIT BOARD");
+            }
+        } else
+        {
+            if (lastGameObject != null) 
+            { 
+            var renderer = lastGameObject.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", lastColor);
+
+            lastGameObject = null;
             }
         }
     }
