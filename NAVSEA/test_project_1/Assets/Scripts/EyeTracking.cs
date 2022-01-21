8189a49c96cc8640b842966f819ed9d1bc3e3f75
thaophan1 <thao_phan@ucsb.edu>
2022-01-20 18:09:10 -0800

    Add eye tracking

--- NAVSEA/test_project_1/Assets/Scripts/EyeTracking.cs
+++ NAVSEA/test_project_1/Assets/Scripts/EyeTracking.cs
@@ -2,32 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
 
 public class EyeTracking : MonoBehaviour
 {
-    private GazeProvider gazeProvider;
     private GameObject lastGameObject;
     private Color lastColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        gazeProvider = new GazeProvider();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*GameObject gazeTarget = gazeProvider.GazeTarget;
-        IMixedRealityInputSource gazeInputSource = gazeProvider.GazeInputSource;
-        Debug.Log("GazeInputSource: " + gazeInputSource.ToString());*/
 
-        /*Debug.Log("IsEyeTrackingDataValid: " + gazeProvider.IsEyeTrackingDataValid);
-        Debug.Log("IsEyeCalibrationValid: " + gazeProvider.IsEyeCalibrationValid);
-        Debug.Log("IsEyeTrackingEnabled: " + gazeProvider.IsEyeTrackingEnabled);
-        Debug.Log("IsEyeTrackingEnabledAndValid: " + gazeProvider.IsEyeTrackingEnabledAndValid);*/
+        GameObject gameObject = CoreServices.InputSystem.EyeGazeProvider.HitInfo.collider.gameObject;
+        if (gameObject != null)
+        {
+            Outline outline;
 
-        RaycastHit hitInfo;
+            if (!gameObject.CompareTag("LightSwitch"))
+            {
+                outline = gameObject.GetComponent<Outline>();
+                outline.OutlineWidth = 2;
+                outline.enabled = true;
+
+                print(gameObject.name + " Outline enabled: " + outline.enabled + " IsActiveAndEnabled: " + outline.isActiveAndEnabled);
+            }
+            else
+            {
+                print(gameObject.name);
+                for (int i = 0; i < gameObject.transform.childCount; i++)
+                {
+                    outline = gameObject.transform.GetChild(i).GetComponent<Outline>();
+                    outline.OutlineWidth = 2;
+                    outline.enabled = true;
+                }
+            }
+
+            if (lastGameObject != null && lastGameObject != gameObject)
+            {
+                if (!lastGameObject.CompareTag("LightSwitch"))
+                {
+                    outline = lastGameObject.GetComponent<Outline>();
+                    outline.enabled = false;
+                }
+                else
+                {
+                    for (int i = 0; i < lastGameObject.transform.childCount; i++)
+                    {
+                        outline = lastGameObject.transform.GetChild(i).GetComponent<Outline>();
+                        outline.enabled = false;
+                    }
+                }
+            }
+
+            lastGameObject = gameObject;
+        }
+        else
+        {
+            if (lastGameObject != null)
+            {
+                Outline outline;
+
+                if (lastGameObject.CompareTag("LightSwich"))
+                {
+                    for (int i = 0; i < lastGameObject.transform.childCount; i++)
+                    {
+                        outline = lastGameObject.transform.GetChild(i).GetComponent<Outline>();
+                        outline.enabled = false;
+                    }
+                }
+                else
+                {
+                    outline = lastGameObject.GetComponent<Outline>();
+                    outline.enabled = false;
+                }
+            }
+        }
+
+
+        // HEAD-GAZE CODE
+
+        /*RaycastHit hitInfo;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo))
         {
             GameObject go = hitInfo.collider.gameObject;
@@ -92,6 +152,6 @@ public class EyeTracking : MonoBehaviour
                     outline.enabled = false;
                 }
             }
-        }
+        }*/
     }
 }
