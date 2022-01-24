e0297a38020d1955167b5a4a9bc9d9d26da48a97
Emily O'Mahony <emilyomahony@gmail.com>
2022-01-23 19:24:19 -0800

    added gesture script to button

---
+++ NAVSEA/test_project_1/Assets/Scripts/TapGestureEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class TapGestureEvent : MonoBehaviour
+{
+    public static GameObject component;
+    private GestureRecognizer recognizer;
+
+    private void Recognizer_Tapped(TappedEventArgs args)
+    {
+        Debug.Log("Tap experienced");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recognizer = new GestureRecognizer();
+        recognizer.Tapped += Recognizer_Tapped;
+        recognizer.SetRecognizableGestures(GestureSettings.Tap); 
+        recognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
