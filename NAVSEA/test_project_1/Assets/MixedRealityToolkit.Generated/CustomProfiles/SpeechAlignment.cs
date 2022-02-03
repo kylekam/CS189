db06849f1cdf58308126cad13666ed977a5bf45f
Kyle Kam <kylekam@ucsb.edu>
2022-02-02 19:19:41 -0800

    Inital automatic alignment implementation

--- NAVSEA/test_project_1/Assets/MixedRealityToolkit.Generated/CustomProfiles/SpeechAlignment.cs
+++ NAVSEA/test_project_1/Assets/MixedRealityToolkit.Generated/CustomProfiles/SpeechAlignment.cs
@@ -11,58 +11,52 @@ public class SpeechAlignment : MonoBehaviour, IMixedRealitySpeechHandler
     public Transform targetPivot;
 
     private int counter = -1;
+    private int moduloCounter;
+
     MixedRealityPose poseRight;
+    MixedRealityPose poseLeftThumb;
+    MixedRealityPose poseLeftPointer;
+
+    public bool alightnmentEnabled;
+    private bool pinchCompleted; // Used so that the pinch is only registered once
 
     // Start is called before the first frame update
     void Start()
     {
         CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
+        alightnmentEnabled = true; // CHANGE AFTER ATTACHING TO INSTRUCTIONS
+        pinchCompleted = false;
     }
 
+    void Update()
+    {
+        if (alightnmentEnabled)
+        {
+            if (HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, Handedness.Left, out poseLeftThumb)
+                && HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Left, out poseLeftPointer)
+                && !pinchCompleted
+                && 0.025 > Vector3.Distance(poseLeftThumb.Position, poseLeftPointer.Position))
+            {
+                tryAlignment();
+                pinchCompleted = true;
+                Debug.Log("pinch!");
+            }
+
+            // Renable pinch after fingers have opened up a bit
+            if (0.045 < Vector3.Distance(poseLeftThumb.Position, poseLeftPointer.Position))
+            {
+                pinchCompleted = false;
+            }
+
+        }
+    }
     public void OnSpeechKeywordRecognized(SpeechEventData eventData)
     {
-        // Get right pointer finger location
-        // Spawn orb
-
         switch (eventData.Command.Keyword.ToLower())
         {
             case "align":
-                Debug.Log("Hello World");
-                if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out poseRight))
-                {
-                    counter++;
-                    int moduloCounter = counter % 5;
-
-                    switch (moduloCounter)
-                    {
-
-                        case 0:
-                            calc.pointA.transform.position = poseRight.Position;
-                            break;
-                        case 1:
-                            calc.pointB.transform.position = poseRight.Position;
-                            break;
-                        case 2:
-                            calc.pointC.transform.position = poseRight.Position;
-                            break;
-                        case 3:
-                            calc.MakeTriangle();
-                            targetPivot.position = calc.center.transform.position;
-                            targetPivot.rotation = calc.center.transform.rotation;
-                            break;
-                        case 4:
-                            //Reset everything
-                            targetPivot.rotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), 1);
-                            targetPivot.position = new Vector3(3.5f, 0, 3);
-                            calc.center.transform.position = Vector3.left;
-                            calc.pointA.transform.position = Vector3.left;
-                            calc.pointB.transform.position = Vector3.left;
-                            calc.pointC.transform.position = Vector3.left;
-
-                            break;
-                    }
-
-                }
+                Debug.Log("Word recognized.");
+                tryAlignment();
                 break;
 
             default:
@@ -71,4 +65,42 @@ public class SpeechAlignment : MonoBehaviour, IMixedRealitySpeechHandler
 
         }
     }
+    void tryAlignment()
+    {
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out poseRight))
+        {
+            counter++;
+            moduloCounter = counter % 5;
+
+            // Spawn orb at location of finger
+            switch (moduloCounter)
+            {
+                case 0:
+                    calc.pointA.transform.position = poseRight.Position;
+                    break;
+                case 1:
+                    calc.pointB.transform.position = poseRight.Position;
+                    break;
+                case 2:
+                    calc.pointC.transform.position = poseRight.Position;
+                    break;
+                case 3:
+                    calc.MakeTriangle();
+                    targetPivot.position = calc.center.transform.position;
+                    targetPivot.rotation = calc.center.transform.rotation;
+                    break;
+                case 4:
+                    //Reset everything
+                    targetPivot.rotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), 1);
+                    targetPivot.position = new Vector3(3.5f, 0, 3);
+                    calc.center.transform.position = Vector3.left;
+                    calc.pointA.transform.position = Vector3.left;
+                    calc.pointB.transform.position = Vector3.left;
+                    calc.pointC.transform.position = Vector3.left;
+                    break;
+            }
+        }
+    }
 }
+
+
